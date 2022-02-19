7b5773701351a198fcfff9c120ebccd148b59d3f
eduardotecnologo <eduardotecnologo@hotmail.com>
2022-02-19 00:26:05 -0300

    slices

--- monitor-sites/src/Hello/hello.go
+++ monitor-sites/src/Hello/hello.go
@@ -4,26 +4,33 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoranento = 3
+const delay = 5
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
 
-	//var comando int
-	comando := lerComando()
+	for {
+		exibeMenu()
 
-	switch comando {
-	case 1:
-		initMonitoramento()
-	case 2:
-		fmt.Println("\nExibindo Logs...")
-	case 3:
-		fmt.Println("\nSaindo...")
-		os.Exit(0)
-	default:
-		fmt.Println("\nNão conheço esse programa...")
-		os.Exit(-1)
+		//var comando int
+		comando := lerComando()
+
+		switch comando {
+		case 1:
+			initMonitoramento()
+		case 2:
+			fmt.Println("\nExibindo Logs...")
+		case 3:
+			fmt.Println("\nSaindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("\nNão conheço esse programa...")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -43,12 +50,37 @@ func exibeMenu() {
 func lerComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
+	fmt.Println(" ")
+
 	return comandoLido
 }
 
 func initMonitoramento() {
 	fmt.Println("\nMonitorando...")
-	site := "https://www.mrs.com.br"
+	sites := []string{
+		"https://www.google.com.br",
+		"https://www.youtube.com"}
+
+	for i := 0; i < monitoranento; i++ {
+		for _, site := range sites {
+			testaSite(site)
+		}
+		for i, site := range sites {
+			fmt.Println("testando", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println(" ")
+		fmt.Println(" ")
+	}
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
 	fmt.Println(resp)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, " foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "site está com Problema", resp.StatusCode)
+	}
 }
